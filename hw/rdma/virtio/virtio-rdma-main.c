21b2ddac95b71dd2ba21e6be88ee75920def57fc
Yuval Shaia <yuval.shaia@oracle.com>
2019-04-02 17:23:02 +0300

    Impl of ib_query_port - cont
    Signed-off-by: Yuval Shaia <yuval.shaia@oracle.com>

--- hw/rdma/virtio/virtio-rdma-main.c
+++ hw/rdma/virtio/virtio-rdma-main.c
@@ -24,17 +24,22 @@
 #include "../rdma_utils.h"
 
 /* TODO: Move to uapi header file */
+#define VIRTIO_RDMA_CTRL_OK    0
+#define VIRTIO_RDMA_CTRL_ERR   1
+
 enum {
-    VIRTIO_CMD_QUERY_DEVICE,
+    VIRTIO_CMD_QUERY_DEVICE = 10,
     VIRTIO_CMD_QUERY_PORT,
 };
+
 struct control_buf {
     uint8_t cmd;
     uint8_t status;
 };
 
-#define VIRTIO_RDMA_CTRL_OK    0
-#define VIRTIO_RDMA_CTRL_ERR   1
+struct cmd_query_port {
+    uint8_t port;
+};
 
 /* TODO: Move to uapi header file */
 
@@ -56,39 +61,45 @@ static int virtio_rdma_query_device(VirtIORdma *rdev, struct iovec *in,
     attr.hw_ver = VIRTIO_RDMA_HW_VER;
     attr.phys_port_cnt = VIRTIO_RDMA_PORT_CNT;
 
-    /* We start with sys_image_guid because of inconsistency beween ib_ and
-     * ibv_ */
     offs = offsetof(struct ibv_device_attr, sys_image_guid);
     s = iov_from_buf(out, 1, 0, (void *)&attr + offs, sizeof(attr) - offs);
-    assert(s == sizeof(attr) - offs);
 
-    return VIRTIO_RDMA_CTRL_OK;
+    return s == sizeof(attr) - offs ? VIRTIO_RDMA_CTRL_OK :
+                                      VIRTIO_RDMA_CTRL_ERR;
 }
+
 static int virtio_rdma_query_port(VirtIORdma *rdev, struct iovec *in,
                                   struct iovec *out)
 {
     struct ibv_port_attr attr = {};
+    struct cmd_query_port cmd = {};
     int offs;
     size_t s;
 
-    attr.state = IBV_PORT_ACTIVE; /* TODO: should be combination of backend and netdev */
+    s = iov_to_buf(in, 1, 0, &cmd, sizeof(cmd));
+    if (s != sizeof(cmd)) {
+        return VIRTIO_RDMA_CTRL_ERR;
+    }
+
+    if (cmd.port != 1) {
+        return VIRTIO_RDMA_CTRL_ERR;
+    }
+
+    attr.state = IBV_PORT_ACTIVE;
     attr.gid_tbl_len = 256;
 
-    /* We start with stat because of inconsistency beween ib and ibv */
     offs = offsetof(struct ibv_port_attr, state);
     s = iov_from_buf(out, 1, 0, (void *)&attr + offs, sizeof(attr) - offs);
-    assert(s == sizeof(attr) - offs);
 
-    return VIRTIO_RDMA_CTRL_OK;
+    return s == sizeof(attr) - offs ? VIRTIO_RDMA_CTRL_OK :
+                                      VIRTIO_RDMA_CTRL_ERR;
 }
 /* TODO: Move to virtio-rdma-ib.c */
 
 static void virtio_rdma_handle_ctrl(VirtIODevice *vdev, VirtQueue *vq)
 {
     VirtIORdma *r = VIRTIO_RDMA(vdev);
-    struct iovec *iov, *iov2;
     struct control_buf cb;
-    unsigned int iov_cnt;
     VirtQueueElement *e;
     size_t s;
 
@@ -108,32 +119,31 @@ static void virtio_rdma_handle_ctrl(VirtIODevice *vdev, VirtQueue *vq)
             break;
         }
 
-        iov_cnt = e->out_num; 
-        iov2 = iov = g_memdup(e->out_sg, sizeof(struct iovec) * e->out_num);
-        s = iov_to_buf(iov, iov_cnt, 0, &cb.cmd, sizeof(cb.cmd));
+        s = iov_to_buf(&e->out_sg[0], 1, 0, &cb.cmd, sizeof(cb.cmd));
         if (s != sizeof(cb.cmd)) {
             cb.status = VIRTIO_RDMA_CTRL_ERR;
         } else {
+	printf("cmd=%d\n", cb.cmd);
             switch (cb.cmd) {
             case VIRTIO_CMD_QUERY_DEVICE:
-                cb.status = virtio_rdma_query_device(r, &e->out_sg[0],
+                cb.status = virtio_rdma_query_device(r, &e->out_sg[1],
                                                      &e->in_sg[0]);
                 break;
             case VIRTIO_CMD_QUERY_PORT:
-                cb.status = virtio_rdma_query_port(r, &e->out_sg[0],
+                cb.status = virtio_rdma_query_port(r, &e->out_sg[1],
                                                    &e->in_sg[0]);
                 break;
             default:
                 cb.status = VIRTIO_RDMA_CTRL_ERR;
             }
         }
+        cb.status = VIRTIO_RDMA_CTRL_OK;
         printf("status=%d\n", cb.status);
-        s = iov_from_buf(&e->in_sg[e->in_num - 1], 1, 0, &cb.status, sizeof(cb.status));
+        s = iov_from_buf(&e->in_sg[1], 1, 0, &cb.status, sizeof(cb.status));
         assert(s == sizeof(cb.status));
 
         virtqueue_push(vq, e, sizeof(cb.status));
         virtio_notify(vdev, vq);
-        g_free(iov2);
     }
 
     virtio_queue_set_notification(vq, 1);
