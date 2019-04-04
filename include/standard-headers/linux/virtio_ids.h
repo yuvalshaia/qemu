161cc2dbbd05a4a4747ac6ec29f5444ceb51c308
Yuval Shaia <yuval.shaia@oracle.com>
2019-04-04 09:39:15 +0300

    change virtio device-id to 26
    Signed-off-by: Yuval Shaia <yuval.shaia@oracle.com>

--- include/standard-headers/linux/virtio_ids.h
+++ include/standard-headers/linux/virtio_ids.h
@@ -43,6 +43,6 @@
 #define VIRTIO_ID_INPUT        18 /* virtio input */
 #define VIRTIO_ID_VSOCK        19 /* virtio vsock transport */
 #define VIRTIO_ID_CRYPTO       20 /* virtio crypto */
-#define VIRTIO_ID_RDMA         21 /* virtio crypto */
+#define VIRTIO_ID_RDMA         26 /* virtio crypto */
 
 #endif /* _LINUX_VIRTIO_IDS_H */
